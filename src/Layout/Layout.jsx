c35239c3566f76098d38e407701761e46bc8df59
ahmod001 <ahmodh08@gmail.com>
2023-12-07 18:38:04 +0600

    Layout setuped

---
+++ src/Layout/Layout.jsx
@@ -0,0 +1,18 @@
+import { } from 'react';
+import Footer from '../Components/Footer/Footer';
+import Header from '../Components/Header/Header';
+import Calculator from '../Components/Calculator/Calculator';
+
+const Layout = () => {
+    return (
+        <main className='bg-cyan-100' >
+            <section className='min-h-screen pt-20 space-y-16'>
+                <Header />
+                <Calculator />
+            </section>
+            <Footer />
+        </main>
+    );
+};
+
+export default Layout;
