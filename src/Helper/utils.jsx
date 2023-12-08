42ac6545f14902e0ca29cf04f5a49810ae681e3a
ahmod001 <ahmodh08@gmail.com>
2023-12-08 14:32:55 +0600

    Debounce method added

--- src/Helper/utils.jsx
+++ src/Helper/utils.jsx
@@ -6,3 +6,15 @@ export const sanitizeInput = (e) => {
 
     inputElement.value = sanitizedValue;
 }
+
+export const debounce = (fn, delay) => {
+    let timeout;
+
+    return (...args) => {
+        clearTimeout(timeout);
+        
+        timeout = setTimeout(() => {
+            fn(...args);
+        }, delay)
+    }
+}
