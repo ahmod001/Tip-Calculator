fa64f431db3b3340d7b5b3197d9e6120cf197623
ahmod001 <ahmodh08@gmail.com>
2023-12-08 17:44:34 +0600

    footer updated

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -2,8 +2,8 @@ import { } from 'react';
 
 const Footer = () => {
     return (
-        <footer className='text-cyan-800 font-medium py-3 text-center'>
-            <p>Developed by <a target='blank' className='underline' href="https://github.com/ahmod001">Ahmod Hasan</a></p>
+        <footer className='text-cyan-800 font-medium py-3 mt-10 text-center'>
+            <p>Developed by <a target='blank' className='underline font-medium' href="https://github.com/ahmod001">Ahmod Hasan</a></p>
         </footer>
     );
 };
